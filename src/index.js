1e63623e7ce56f2dfb1ae3889e9b0afcf01d5ebe
divyanshuan <vermadivyanshu000@gmail.com>
2023-03-16 20:33:00 +0530

    animation added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Home from "./components/home/index.js";
 import Skill from "./components/skills";
 import Project from "./components/project";
 import About from "./components/about";
-import Chatinetface from "./components/chat/chatbtn";
+// import Chatinetface from "./components/chat/chatbtn";
 import "animate.css/animate.min.css";
 import "aos/dist/aos.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -25,7 +25,3 @@ root.render(
     }
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
