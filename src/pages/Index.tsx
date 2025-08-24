b2ddbd69e417f5b58a705fa27d62089215cfe1fa
Kshitiz Sikriwal <143925814+Kshitizsikriwal@users.noreply.github.com>
2025-08-25 03:10:43 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
-import Certificates from "@/components/certificates";
+import Certificates from "@/components/Certificates";
 import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
